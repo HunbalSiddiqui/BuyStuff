a21afbd76117e0af9b7324cbcc9d8b45377c892e
hunbalSiddiqui <hunbal.dha@gmail.com>
2020-07-06 21:12:43 +0500

    Backend : Models Completed

---
+++ models/product.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const {Schema,ObjectId} = mongoose.Schema;
+
+
+var productSchema = new Schema({
+    name : {
+        type : String,required : true,
+        trim : true,maxlength : 32
+    },
+    description : {
+        type : String, required : true,
+        trim : true,maxlength : 2000
+    },
+    price : {
+        type : Number, trim : true,
+        required : true,maxlength : 32 
+    },
+    category : {
+        type : ObjectId,
+        ref : "Category",
+        require : true
+    },
+    stock : {
+        type : Number
+    },
+    sold : {
+        type : Number,
+        default : 0
+    },
+    photo : {
+        data : Buffer,
+        contentType : String,
+    }
+},{timestamps : true})
+
+module.exports = mongoose.model("Product", productSchema)
